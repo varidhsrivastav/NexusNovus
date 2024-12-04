324413afee3da84ac819ba5b20de3541d1995abb
varidhsrivastav <varidhsrivastava19145@gmail.com>
2024-12-04 22:36:12 +0530

    configuring the site and onboarding

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      'uploadthing.com',
+      'utfs.io',
+      'img.clerk.com',
+      'subdomain',
+      'files.stripe.com',
+    ],
+  },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
