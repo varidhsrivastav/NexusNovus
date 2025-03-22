d1a9c546640922e905923b78a05aadc7406e1dc8
varidhsrivastav <varidhsrivastava19145@gmail.com>
2025-03-22 16:32:57 +0530

    uodated favicon

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -2,19 +2,14 @@ import AgencyDetails from "@/components/forms/agency-details";
 import { getAuthUserDetails } from "@/lib/queries";
 import { verifyAndAcceptInvitation } from "@/lib/queries";
 import { currentUser } from "@clerk/nextjs/server";
-import { Plan } from "@prisma/client";
 import { redirect } from "next/navigation";
 import React from "react";
 
-const Page = async ({
-  searchParams,
-}: {
-  searchParams: { plan: Plan; state: string; code: string };
-}) => {
+const Page = async ({ searchParams }: any) => {
   const agencyId = await verifyAndAcceptInvitation();
   console.log(agencyId);
 
-  //get the users details
+  // Get the user's details
   const user = await getAuthUserDetails();
   if (agencyId) {
     if (user?.role === "SUBACCOUNT_GUEST" || user?.role === "SUBACCOUNT_USER") {
@@ -26,8 +21,7 @@ const Page = async ({
         );
       }
       if (searchParams.state) {
-        const statePath = searchParams.state.split("___")[0];
-        const stateAgencyId = searchParams.state.split("___")[1];
+        const [statePath, stateAgencyId] = searchParams.state.split("___");
         if (!stateAgencyId) return <div>Not authorized</div>;
         return redirect(
           `/agency/${stateAgencyId}/${statePath}?code=${searchParams.code}`,
@@ -37,12 +31,14 @@ const Page = async ({
       return <div>Not authorized</div>;
     }
   }
+
   const authUser = await currentUser();
   if (!authUser) return redirect("/sign-in");
+
   return (
     <div className="flex justify-center items-center mt-4">
       <div className="max-w-[850px] border-[1px] p-4 rounded-xl">
-        <h1 className="text-4xl"> Create An Agency</h1>
+        <h1 className="text-4xl">Create An Agency</h1>
         <AgencyDetails
           data={{ companyEmail: authUser?.emailAddresses[0].emailAddress }}
         />
